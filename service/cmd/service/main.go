d7a4cae99f10070f3da387fe18d00bac844acc1d
AvaNdrey <avandrey.work@gmail.com>
2023-09-22 14:51:49 +0300

    Done saving orders in database and cache

--- service/cmd/service/main.go
+++ service/cmd/service/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	order "service/internal/order"
 
@@ -26,32 +27,74 @@ const (
 	subject   = "order-channel"
 )
 
+var cache = make(map[string]order.OrderDTO)
+
 func main() {
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	dbPort, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbName := os.Getenv("DB_NAME")
 
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
 	db, err := sql.Open("postgres", connStr)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	log.Println("INFO postgres: Successfully conected to database")
+	log.Printf("INFO postgres: Successfully conected to database\n%s\n", connStr)
+	rows, err := db.Query("SELECT data FROM orders")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var orderJSON []byte
+		if err := rows.Scan(&orderJSON); err != nil {
+			log.Fatal(err)
+		}
+
+		newOrder := order.OrderDTO{}
+		if err := json.Unmarshal(orderJSON, &newOrder); err != nil {
+			log.Fatal(err)
+		}
+
+		cache[newOrder.OrderUID] = newOrder
+		fmt.Printf("INFO cache: From postgres order %s\n", newOrder.OrderUID)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		query := r.URL.Query()
 		id := query.Get("id")
-		log.Println("INFO server: GET http://localhost:8080/ -> 200 OK")
-
-		data := DataJSON{200, id}
-		res, err := json.Marshal(data)
-		if err != nil {
-			log.Fatal(err)
+		var res []byte
+		var err error
+		if data, ok := cache[id]; ok {
+			res, err = json.Marshal(data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("INFO server: GET http://localhost:8080/ -> 200 OK")
+		} else {
+			data := struct {
+				Code    int    `json:"code"`
+				Message string `json:"message"`
+			}{
+				Code:    404,
+				Message: "Order not found",
+			}
+			res, err = json.Marshal(data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("INFO server: GET http://localhost:8080/ -> 404 NOT FOUND")
 		}
 
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -74,6 +117,17 @@ func main() {
 		if err := json.Unmarshal(msg.Data, &newOrder); err != nil {
 			log.Fatal(err)
 		}
+		cache[newOrder.OrderUID] = newOrder
+		dbOrder, err := json.Marshal(newOrder)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec("INSERT INTO orders(data) VALUES($1)", dbOrder)
+		if err != nil {
+			log.Fatalf("INSERT INTO orders(data) VALUES($1) error: %s", err)
+		}
+
 		fmt.Printf("INFO nats: Received order %s\n", newOrder.OrderUID)
 	})
 	if err != nil {
