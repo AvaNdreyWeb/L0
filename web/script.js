c81908de0493a4da13c275ec4c04c57d93391211
AvaNdrey <avandrey.work@gmail.com>
2023-09-23 16:26:57 +0300

    Catching service shutdown

--- web/script.js
+++ web/script.js
@@ -2,16 +2,21 @@ const form = document.querySelector('.get-by-id-form');
 const input = document.querySelector('.id');
 const output = document.querySelector('.order');
 
+const serverError = {code: 500, message: "Сервис временно недоступен"}
+
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const ID = input.value;
-    fetch(`http://localhost:8080/?id=${encodeURIComponent(ID)}`)
+    const ID = input.value.trim();
+    if (ID != '') {
+        fetch(`http://localhost:8080/?id=${encodeURIComponent(ID)}`)
         .then(response => response.json())
         .then(jsonResult => {
             output.value = JSON.stringify(jsonResult, null, 2);
         })
         .catch(error => {
-            console.error('Error:', error);
+            output.value = JSON.stringify(serverError, null, 2);
         });
+    }
+    
 });
